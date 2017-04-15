16e8757da1f947c2d64b2066579b6389765c3c23
Walden-Shen <41880356@qq.com>
2017-04-15 20:20:37 +0800

    vol 0.2(plus function)

--- myClient.c
+++ myClient.c
@@ -1,4 +1,13 @@
 #include "unp.h"
+struct args{
+	long arg1;
+	long arg2;
+};
+struct result{
+	long sum;
+};
+void str_cli(FILE* fp, int sockfd);
+void sum_cli(FILE* fp, int sockfd);
 int main(int argc, char** argv){
 	char recvline[MAXLINE + 1];
 	int sockfd, n;
@@ -6,30 +15,45 @@ int main(int argc, char** argv){
 	if(argc != 2){
 		err_quit("usage error");
 	}
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-		err_sys("socket error");
-	}
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(13);
-	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
-		err_sys("ip address invalid");
-	}
-	if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
-		err_sys("unable to connect");
-	}
-	while((n = read(sockfd, recvline, MAXLINE)) > 0){
-		recvline[n] = 0;
-		if(fputs(recvline, stdout) == EOF){
-			err_sys("fputs error");
+	servaddr.sin_port = htons(SERV_PORT);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	Connect(sockfd, (SA*) &servaddr, sizeof(servaddr));
+	sum_cli(stdin, sockfd);
+	exit(0);
+}
+void sum_cli(FILE* fp, int sockfd){
+	char sendline[MAXLINE];
+	struct args args;
+	struct result result;
+	while(Fgets(sendline, MAXLINE, fp) != NULL){
+		if(sscanf(sendline, "%ld%ld", &args.arg1, &args.arg2) != 2){
+			Fputs("error with input", stdout);
+			continue;
 		}
+		Writen(sockfd, &args, sizeof(args));
+		if(Readn(sockfd, &result, sizeof(result)) == 0){
+			err_quit("terminate prematurely");
+		}
+		printf("%ld\n", result.sum);
 	}
-	if(n < 0){
-		err_sys("read error");
-	}
-	close(sockfd);
+	return;
 }
 
+
+void str_cli(FILE* fp, int sockfd){
+	char sendline[MAXLINE], recvline[MAXLINE];
+	while(Fgets(sendline, MAXLINE, fp) != NULL){
+		Writen(sockfd, sendline, strlen(sendline));
+		if(Readline(sockfd, recvline, MAXLINE) == 0){
+			err_quit("server terminate prematurely");
+		}
+		Fputs(recvline, stdout);
+	}
+}
+	
 
 
 
--- myServer.c
+++ myServer.c
@@ -1,32 +1,77 @@
 #include "unp.h"
+struct args{
+	long arg1;
+	long arg2;
+};
+struct result{
+	long sum;
+};
+void sum_serv(int sockfd);
 void doit(int connfd);
 int sockfd_to_family(int sockfd);
+void str_echo(int sockfd);
+void sig_chld(int signo);
 int main(int argc, char **argv){
 	int listenfd, connfd;
+	pid_t childpid;
 	char buff[MAXLINE];
-	socklen_t len;
+	socklen_t clilen;
 	struct sockaddr_in servaddr, cliaddr;
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_port = htons(13);
+	servaddr.sin_port = htons(SERV_PORT);
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	Bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 	Listen(listenfd, LISTENQ);
+	Signal(SIGCHLD, sig_chld);
 	while(1){
-		len = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA*)&cliaddr, &len);
-		printf("%d\n", sockfd_to_family(connfd));
+		clilen = sizeof(cliaddr);
+		connfd = Accept(listenfd, (SA*)&cliaddr, &clilen);
+//		Getsockname(connfd, (SA*)&cliaddr, &len);
+//		printf("local addr :%s\n", Sock_ntop((SA*) &cliaddr, len));
+//		printf("%d\n", sockfd_to_family(connfd));
 		printf("connenction from %s, port %d\n", Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)), ntohs(cliaddr.sin_port));
-		if(fork() == 0){
+		if((childpid = fork()) == 0){
 			Close(listenfd);
-			doit(connfd);
-			Close(connfd);
+			sum_serv(connfd);
 			exit(0);
-		}
+		}	
 		Close(connfd);
 	}
 }
+void sum_serv(int sockfd){
+	struct args args;
+	struct result result;
+	while(Readn(sockfd, &args, sizeof(args)) > 0){
+		result.sum = args.arg1 + args.arg2;
+		Writen(sockfd, &result, sizeof(result));
+	}
+	return;
+}
+
+void sig_chld(int signo){
+	pid_t pid;
+	int stat;
+	while((pid = waitpid(-1, &stat, WNOHANG)) > 0){		
+		printf("child %d terminated\n", pid);
+	}	
+	return;
+}
+void str_echo(int sockfd){
+	char buf[MAXLINE];
+	ssize_t n;
+again:
+	while((n = read(sockfd, buf, MAXLINE)) > 0){
+		Writen(sockfd, buf, n);
+	}
+	if(n < 0 && errno == EINTR){
+		goto again;
+	}else if(n < 0){
+		err_sys("read error");
+	}
+}
+
 void doit(int connfd){
 	char buff[MAXLINE] = "test for once\n";
 	write(connfd, buff, strlen(buff));
